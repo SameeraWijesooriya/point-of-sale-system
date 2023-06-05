207e1a4cb86fc3f6024803f42c0c01e53c52ccfd
sameera <sameeram.wijesooriya95@gmail.com>
2023-06-06 01:01:14 +0530

    add my project repository

---
+++ back-end/src/main/java/lk/ijse/dep10/pos/WebRootConfig.java
@@ -0,0 +1,21 @@
+package lk.ijse.dep10.pos;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebRootConfig {
+
+    @Bean
+    public BasicDataSource dataSource(){
+        BasicDataSource bds = new BasicDataSource();
+        bds.setUsername("root");
+        bds.setPassword("1234");
+        bds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        bds.setUrl("jdbc:mysql://localhost:3306/dep10_pos_web?createDatabaseIfNotExist=true");
+        bds.setMaxTotal(50);
+        bds.setInitialSize(20);
+        return bds;
+    }
+}
